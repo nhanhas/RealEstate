ac4d190e307ccf925c7d2a5a651ada097d343354
nhanhas <mpereira132@hotmail.com>
2018-05-13 18:35:56 +0100

    S#4 - Some working set

--- sections/navbar/navbar.js
+++ sections/navbar/navbar.js
@@ -1,2 +1,17 @@
 const settings = require('electron-settings')
 
+const filterBtn = document.getElementById('filter-btn');
+
+//#1 - Add Event Listener on click to popup de dialog with filters
+filterBtn.addEventListener('click', (event) => {
+    //#1 - First we get the filters container
+    const filterContainer = document.getElementById('filter-container');
+    
+    //#2 - Check if filter container is hidden
+    if(filterContainer.classList.contains('is-hidden')){
+        document.getElementById(filterContainer.id).classList.remove('is-hidden');
+    }else{
+        document.getElementById(filterContainer.id).classList.add('is-hidden');
+    }
+
+});
